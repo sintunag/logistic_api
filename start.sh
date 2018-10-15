9f9089062bc6dd48c6f2706bac5b6dcc10f74269
sintu roy <sintu.roy@nagarro.com>
2018-10-15 15:41:18 +0530

    updated batch file with npm installation

--- start.sh
+++ start.sh
@@ -18,6 +18,16 @@ else
     echo "Docker Compose already installed.  Skipping..."
 fi
 
+# Installing npm to testcase
+if [ ! -f /usr/bin/npm ]; then
+    echo 'Installing npm'
+    # Install npm from the Debian-based distributions repository
+    sudo apt-get install -y npm
+
+else
+    echo "npm already installed.  Skipping..."
+fi
+
 # # Start the application
 echo 'Starting application'
 docker-compose up &
